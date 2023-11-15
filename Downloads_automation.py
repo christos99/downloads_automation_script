8c1ece06d58540974b04758b600f0919a42ffdb3
christos99 <mangas.99@windowslive.com>
2023-11-15 12:46:30 +0200

    added more Folders in the Downloads changed the internal in com.plist

--- Downloads_automation.py
+++ Downloads_automation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# /opt/homebrew/bin/python3
 
 
 # This shebang line allows the script to be run as a standalone executable.
@@ -7,16 +7,21 @@ import os  # Used for file system path operations
 import shutil  # Used for file operations like moving files
 
 # Specify the directory where downloaded files are stored
-downloads_path = '/Users/christos/Downloads'
+downloads_path_1 = '/Users/christos/Downloads'
+downloads_path_2 = '/Users/christos/Desktop'
+
 # Define a dictionary that maps file extensions to their new destination folders
 destinations = {
     'pdf': '/Users/christos/Downloads/Downloaded_Documents',
+    'epub': '/Users/christos/Downloads/Downloaded_Books',
     'txt': '/Users/christos/Downloads/Downloaded_Documents',
     'jpg': '/Users/christos/Downloads/Downloaded_Images',
+    'jpeg': '/Users/christos/Downloads/Downloaded_Images',
+    'HEIC': '/Users/christos/Downloads/Downloaded_Images',
     'png': '/Users/christos/Downloads/Downloaded_Images',
-    'mp3': '/Users/christos/Downloads/Downloaded_Music',   # Adding music files
-    'mp4': '/Users/christos/Downloads/Downloaded_Videos',   # Adding video files
-    'docx': '/Users/christos/Downloads/Downloaded_Documents',    # Adding Word documents
+    'mp3': '/Users/christos/Downloads/Downloaded_Music',  # Adding music files
+    'mp4': '/Users/christos/Downloads/Downloaded_Videos',  # Adding video files
+    'docx': '/Users/christos/Downloads/Downloaded_Documents',  # Adding Word documents
     'xlsx': '/Users/christos/Downloads/Downloaded_Documents',
     # Add more file types and destinations as needed
 }
@@ -50,10 +55,13 @@ def sort_and_move_files(downloads_path, destinations):
                 # Notify user if no destination is found for the file type
                 print(f"No destination found for: {filename}. File is ignored.")
 
+
 # Wrap the function call in a try-except block to handle potential exceptions
 try:
     # Call the function to start sorting and moving files
-    sort_and_move_files(downloads_path, destinations)
+    sort_and_move_files(downloads_path_1, destinations)
+    sort_and_move_files(downloads_path_2, destinations)
+
 except Exception as e:
     # Print any error that occurs during the file operation
     print(f"An error occurred: {e}")
